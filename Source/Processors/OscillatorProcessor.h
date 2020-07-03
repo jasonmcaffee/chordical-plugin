77414f40ba867a3ed038d388de7299190c7037ec
jason mcaffee <jasonlmcaffee@gmail.com>
2020-07-03 17:59:19 -0600

    oscillator processor wip.

--- Source/Processors/OscillatorProcessor.h
+++ Source/Processors/OscillatorProcessor.h
@@ -2,6 +2,16 @@
 
 class OscillatorProcessor : public ProcessorBase{
 public:
+    OscillatorProcessor(){
+        oscillator.setFrequency(440.0f);
+        oscillator.initialise([](float x) { return std::sin (x); });
+    }
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override {
+
+    }
+
+
     const juce::String getName() const override { return "Oscillator"; }
 private:
     juce::dsp::Oscillator<float> oscillator;
