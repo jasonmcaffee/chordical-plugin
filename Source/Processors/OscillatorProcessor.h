b8b3ce65c9403528511209891283bea934b1bfa7
jason mcaffee <jasonlmcaffee@gmail.com>
2020-07-03 20:06:12 -0600

    oscillator processor implemented.

--- Source/Processors/OscillatorProcessor.h
+++ Source/Processors/OscillatorProcessor.h
@@ -8,11 +8,23 @@ public:
     }
 
     void prepareToPlay(double sampleRate, int samplesPerBlock) override {
-
+        //create a spec for the oscillator
+        juce::dsp::ProcessSpec spec { sampleRate, static_cast<juce::uint32>(samplesPerBlock) };
+        oscillator.prepare(spec);
     }
 
+    void processBlock(juce::AudioSampleBuffer & buffer, juce::MidiBuffer&) override {
+        juce::dsp::AudioBlock<float> block (buffer);
+        juce::dsp::ProcessContextReplacing<float> context (block);
+        oscillator.process(context);
+    }
+
+    void reset() override {
+        oscillator.reset();
+    }
 
     const juce::String getName() const override { return "Oscillator"; }
+
 private:
     juce::dsp::Oscillator<float> oscillator;
 };
