2289c517d3c392970f4a66b70a7eb9a12f2cf1a2
jason mcaffee <jasonlmcaffee@gmail.com>
2020-12-22 10:18:02 -0700

    project files

---
+++ Builds/CLion/cmake-build-debug/CMakeFiles/VST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VST.dir/Users/jason/dev/chordical-plugin/JuceLibraryCode/include_juce_audio_plugin_client_VST2.cpp.o"
+  "chordical-plugin.vst/Contents/./PkgInfo"
+  "chordical-plugin.vst/Contents/MacOS/chordical-plugin"
+  "chordical-plugin.vst/Contents/MacOS/chordical-plugin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
