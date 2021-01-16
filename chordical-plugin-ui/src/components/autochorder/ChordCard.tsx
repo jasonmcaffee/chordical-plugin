ffb11fab3a63c3e72737689f9d9974a3b90829e8
jasonmcaffee <jasonlmcaffee@gmail.com>
2021-01-16 11:43:24 -0700

    note select options are now sorted by whether they are in the key. when in key, they have a different background color.

--- chordical-plugin-ui/src/components/autochorder/ChordCard.tsx
+++ chordical-plugin-ui/src/components/autochorder/ChordCard.tsx
@@ -7,34 +7,36 @@ import Select from "../common/Select";
 import {IPredefinedNote} from "../../services/music/predefinedNotes";
 import {ISelectOption} from "../../models/view/common/ISelectOption";
 import Button from "../common/Button";
+import {INoteSelectOption} from "../../services/music/notes";
 
 export default function ChordCard({chord, viewModel}: {chord: IChord, viewModel: IAutochorderPageViewModel}){
   const chordTypeOptions2 = autochorder.getChordTypeSelectOptions({chordRootNote: chord.rootNote});
-  const chordTypeOptions = chordTypeOptions2.map(c => ({...c, className: c.isInScale ? "in-scale" : "not-in-scale"}) );
+  const chordTypeOptions = chordTypeOptions2.map(c => ({...c, className: c.isInScale ? "in-key" : "not-in-key"}) );
   const currentlySelectChordTypeOption = chordTypeOptions.find(c => c.label === chord.type.toString());
-  const currentlySelectedChordRootNote = viewModel.noteSelectOptions.find(o => o.value === chord.rootNote);
+  const noteSelectOptions = viewModel.noteSelectOptions.map(o => ({...o, className: o.isInKey ? "in-key": "not-in-key"}));
+  const currentlySelectedChordRootNote = noteSelectOptions.find(o => o.value === chord.rootNote);
   const key = chord.id;
   return <div key={key} className="chord-card">
     <div className="details">
       <div className="label" onMouseDown={() => autochorder.playChord({chord})} onMouseUp={() => autochorder.stopChord({chord})}>{chord.label}</div>
-      <Select currentlySelectedOption={currentlySelectedChordRootNote} options={viewModel.noteSelectOptions} onChange={(o)=> autochorder.changeChordRootNote({chord, newRootNote: o.value})}/>
+      <Select currentlySelectedOption={currentlySelectedChordRootNote} options={noteSelectOptions} onChange={(o)=> autochorder.changeChordRootNote({chord, newRootNote: o.value})}/>
       <Select currentlySelectedOption={currentlySelectChordTypeOption} options={chordTypeOptions} onChange={(o) => autochorder.changeChordType({chord, newChordType: o.value})}/>
     </div>
     <div className="notes">
       <Button label={"randomize"} onClick={() => { autochorder.randomizeNoteVoicingForChordAndSample({chord}); } }/>
-      {createSelectForEachNoteInChord({chord, viewModel})}
+      {createSelectForEachNoteInChord({chord, viewModel, noteSelectOptions})}
     </div>
    </div>;
 }
 
-function createSelectForEachNoteInChord({chord, viewModel}: {chord: IChord, viewModel: IAutochorderPageViewModel}){
-  return chord.notes.map(note => createSelectForNote({note, chord, viewModel}));
+function createSelectForEachNoteInChord({chord, viewModel, noteSelectOptions}: {chord: IChord, viewModel: IAutochorderPageViewModel, noteSelectOptions: INoteSelectOption[]}){
+  return chord.notes.map(note => createSelectForNote({note, chord, viewModel, noteSelectOptions}));
 }
 
-function createSelectForNote({note, chord, viewModel}: {note: IPredefinedNote, chord: IChord, viewModel: IAutochorderPageViewModel}){
+function createSelectForNote({note, chord, viewModel, noteSelectOptions}: {note: IPredefinedNote, chord: IChord, viewModel: IAutochorderPageViewModel, noteSelectOptions: INoteSelectOption[]}){
   // const selectOptionForNote = viewModel.noteSelectOptions.find(s => s.value === note.noteSymbol);
   const selectedOptionForOctave = viewModel.octaveOptions.find(o => o.value === note.octave);
-  const noteSelectOptions = viewModel.noteSelectOptions.map(o => ({...o, className: o.isInKey ? "in-key": "not-in-key"}));
+  // const noteSelectOptions = viewModel.noteSelectOptions.map(o => ({...o, className: o.isInKey ? "in-key": "not-in-key"}));
   const selectOptionForNote = noteSelectOptions.find(s => s.value === note.noteSymbol);
   // console.error(`chord is:`, chord, selectedOptionForOctave);
   return <div className="note-and-octave">
